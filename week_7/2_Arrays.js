8164b5947b7d0e47d9994ef6ccc8a955d4ac1a64
kaushal <megamindruler@gmail.com>
2023-05-21 17:08:12 +0530

    # Arrays

--- week_7/2_Arrays.js
+++ week_7/2_Arrays.js
@@ -4,3 +4,14 @@
 // Removing Data From Arrays
 // Finding length of Array
 // Nested Arrays
+
+// Arrays are  comma seperated list of values (number,string, arrays, objects , )enclosed in square bracket
+//
+// and Array belongs to object structural datatype in javascript.
+// how to differentiate between object and array?
+// 
+// 
+//every element in an array has sequencial number known as index number. 
+//length of an array = last index number +1;
+
+// 
