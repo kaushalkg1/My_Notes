7b181ae86f003aadce68271d00bdba3e797c8918
kaushal <megamindruler@gmail.com>
2023-05-23 21:36:33 +0530

    JSX

--- week_7/2_Arrays.js
+++ week_7/2_Arrays.js
@@ -13,5 +13,15 @@
 // 
 //every element in an array has sequencial number known as index number. 
 //length of an array = last index number +1;
-
 // 
+// 
+let cake= ['black forest','Pineapple','Butterscotch','Mango']
+cake.push('vanilla');// push -adds element after last index of array.
+cake[1]='Raspberry'
+
+cake.pop()// pop - removes element from last index.
+cake[8]='Cocoa'; // if index is beyond arry length it creates empty arrays elements for remaining indexes .
+// cake.pop()
+// cake.pop()
+console.log(`I like ${cake[cake.length-1]}`)
+console.log(cake)   
